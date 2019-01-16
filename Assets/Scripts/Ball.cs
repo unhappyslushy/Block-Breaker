9bc81ae253fb1c1fde8af14d5995afdc1f73316a
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2019-01-16 15:03:23 -0700

    Ball is attached now

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,12 +6,15 @@ public class Ball : MonoBehaviour {
 
 	[SerializeField] Paddle paddle1;
 	// Use this for initialization
+
+	Vector2 paddleToBallVector;
 	void Start () {
-		
+		paddleToBallVector = transform.position - paddle1.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+		transform.position = paddlePos + paddleToBallVector;
 	}
 }
