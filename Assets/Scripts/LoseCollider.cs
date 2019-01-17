997939b06d0c1a6c006eff317f19eb02103f989e
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2019-01-17 15:10:07 -0700

    JT

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -6,7 +6,7 @@ public class LoseCollider : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		SceneManager.LoadScene("Game Over");
+		SceneManager.LoadScene("End Scene");
 	}
 
 }
