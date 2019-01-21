1395883dbb8e769b2cd8f625acaf338e4a2b056e
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2019-01-21 14:49:59 -0700

    Final Commit

--- Assets/Scripts/block.cs
+++ Assets/Scripts/block.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class block : MonoBehaviour {
+	Level level;
 
-
+	private void Start()
+	{
+		level = FindObjectOfType<Level>();
+		level.CountBreakableBlocks();
+	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		Destroy(gameObject);
---
+++ Assets/Scripts/level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+	[SerializeField] int breakableBlocks;
+	[SerializeField] Level level;
+	public void CountBreakableBlocks()
+	{
+		breakableBlocks++;
+	}
+
+}
